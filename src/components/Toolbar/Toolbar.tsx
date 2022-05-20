469c3a393472d18840cba37d5d53c0b7b33ae5f6
tigawanna <denniskinuthiaw@gmail.com>
2022-05-20 14:25:32 +0300

    revert "starter files"
    This reverts commit 4a6f09ade34b9a99891a1e0eaebb06fb4b8055be.

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,41 +1,24 @@
 import React from 'react'
 import { GrHome } from "react-icons/gr";
 import { IconContext } from "react-icons/lib";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaUserCircle } from 'react-icons/fa';
-import { useAuthSignOut} from '@react-query-firebase/auth';
-import { auth } from '../../firebase/firebaseConfig';
-
-import { UserState } from './../../utils/auth/types';
-import { User } from 'firebase/auth';
 interface ToolbarProps {
-user?:User|null
+
 }
 
-export const Toolbar: React.FC<ToolbarProps> = ({user}) => {
-const mutation = useAuthSignOut(auth);
-const userImg =user?.photoURL;
-const image =userImg?userImg:'https://picsum.photos/id/237/100/100'
+export const Toolbar: React.FC<ToolbarProps> = () => {
 return (
  <div className='w-full bg-slate-500 h-16'>
 <IconContext.Provider value={{ size: "25px", className: "table-edit-icons" }} >
  <div className='flex flex-grow flex-3'>
-<div className='m-1 w-full p-3 bg-slate-800'>
+     <div className='m-1 w-full p-3 bg-slate-800'>
      <Link to="/"><GrHome /></Link>
      </div>
      <div className='m-1 w-full p-3 bg-slate-600'>
      <Link to="/project">Project</Link>
      </div>
-     <div className='m-1 w-fit p-3 bg-slate-700'>
-      {!user?<FaUserCircle />
-       :<img  
-    //   @ts-ignore 
-       src={image} 
-       alt={'no'}
-       onClick={()=>{mutation.mutate()}}
-       className="rounded-[50%] h-full  w-[70px]"
-       />}
-     </div>
+     <div className='m-1 w-fit p-3 bg-slate-700'><FaUserCircle/></div>
 
 </div>   
 </IconContext.Provider>
--- src/utils/auth/types.ts
+++
@@ -1,6 +0,0 @@
-export interface UserState {
-    displayName:string,
-    email:string,
-    uid:string,
-    photoURL:string
-}
